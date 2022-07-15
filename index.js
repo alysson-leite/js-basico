ad624bc2ae5a073b673232c587cfa21b5007bebb
Alysson Leite <alysson_leite@yahoo.com.br>
2022-07-15 16:16:36 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// === SOMA ===
+
+function soma(numero1, numero2) {
+    console.log(numero1 + numero2)
+}
+
+soma(2, 7)
+
+
+
+// === SUBTRAÇÃO ===
+
+function subtracao(numero3, numero4) {
+    console.log(numero3 - numero4)
+}
+
+subtracao(5, 2)
+
+
+
+// === MULTIPLICAÇÃO ===
+
+function multiplicacao(numero5, numero6) {
+    console.log(numero5 * numero6)
+}
+
+multiplicacao(2, 5)
+
+
+
+// === DIVISÃO ===
+
+function divisao(numero7, numero8) {
+    console.log(numero7 / numero8)
+}
+
+divisao(20, 4)
